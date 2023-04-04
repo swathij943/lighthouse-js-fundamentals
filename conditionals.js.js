724bf0d7da32b1a43798cadb559a8ffb79a18276
swathij943 <swathij943@gmail.com>
2023-04-03 20:19:45 -0400

    Added exercise files

---
+++ conditionals.js.js
@@ -0,0 +1,5 @@
+const raining = true;
+if (raining)
+{
+  console.log("Don't forget your umberlla");
+}
---
+++ isOdd.js
@@ -0,0 +1,9 @@
+const isOdd  = function (num) {
+  return num % 2 === 1;
+}
+
+const tenIsOdd = isOdd(10);
+const elevenIsOdd = isOdd(11);
+
+console.log(tenIsOdd);
+console.log(elevenIsOdd);
